6128ca7cfccb3c4e995bb87f63e28303ea787f6b
22CB006 <aryalakshmi.m2022csbs@sece.ac.in>
2024-10-07 15:33:18 +0530

    custom not found page

--- src/app/products/[productId]/review/[reviewId]/page.tsx
+++ src/app/products/[productId]/review/[reviewId]/page.tsx
@@ -1,9 +1,14 @@
+import {notFound} from "next/navigation";
+
 export default function ReviewDetail({params,}:{
      params:{
         productId:string;
         reviewId: string;
      };
 }){
+    if(parseInt(params.reviewId) > 1000){
+        notFound();
+    }
     return (
         <h1>
             Review {params.reviewId} for product {params.productId}
