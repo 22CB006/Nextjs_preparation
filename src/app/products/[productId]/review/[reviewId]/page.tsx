6cf932318b6851d025595d54aac4538b78daf674
22CB006 <aryalakshmi.m2022csbs@sece.ac.in>
2024-10-07 14:25:52 +0530

    Nested dynamic Routes

---
+++ src/app/products/[productId]/review/[reviewId]/page.tsx
@@ -0,0 +1,13 @@
+export default function ReviewDetail({params,}:{
+     params:{
+        productId:string;
+        reviewId: string;
+     };
+}){
+    return (
+        <h1>
+            Review {params.reviewId} for product {params.productId}
+        </h1>
+    );
+
+}
