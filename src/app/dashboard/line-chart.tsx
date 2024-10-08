7b87b5285983cc5528a496b880dc8facd5e1d2a7
22CB006 <aryalakshmi.m2022csbs@sece.ac.in>
2024-10-08 14:29:31 +0530

    File colocation

---
+++ src/app/dashboard/line-chart.tsx
@@ -0,0 +1,5 @@
+export default function LineChart(){
+    return <h1>Line chart</h1>
+}
+
+// page not found - since it is not publicly accessible
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+ function barChart(){
+    return <h1>Bar chart</h1>
+}
+export default function LineChart(){
+    return <h1>Line chart</h1>
+}
+
+//Only the function which is export default will be shown in the page
