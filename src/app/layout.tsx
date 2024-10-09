09b8912b99364c50342a548787dc8a44d9674c3d
22CB006 <aryalakshmi.m2022csbs@sece.ac.in>
2024-10-09 14:31:57 +0530

    Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header style = {{
+          backgroundColor :"lightblue",
+          padding :"1rem",
+        }}
+        >
+          <p>Header</p>
+       </header>
+       {children}
+       <footer style = {{
+          backgroundColor :"pink",
+          padding :"1rem",
+        }}>
+          <p>Footer</p>
+
+       </footer>
+       </body>
     </html>
   )
 }
